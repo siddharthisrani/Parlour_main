ae281efccf7947d4ffc7de0e1e64bf9059769731
siddharthisrani <143181863+siddharthisrani@users.noreply.github.com>
2024-03-07 16:25:48 +0530

    Update App.js

--- parlour/src/App.js
+++ parlour/src/App.js
@@ -26,9 +26,10 @@ const App = () => {
   const [error, setError] = useState('');
   const [showPopup, setShowPopup] = useState(false);
 
+  axios.defaults.withCredentials=true;
   const sendOtp = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/api/send-otp', { name, email });
+      const response = await axios.post('https://parlour-main-api.vercel.app/send-otp', { name, email });
       setMessage(response.data.message);
     } catch (error) {
       setError(error.response?.data?.error || 'An error occurred while sending OTP.');
@@ -37,7 +38,7 @@ const App = () => {
   
   const verifyOtp = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/api/verify-otp', { email, otp: otp.trim() });
+      const response = await axios.post('https://parlour-main-api.vercel.app/verify-otp', { email, otp: otp.trim() });
       setMessage(response.data.message);
       setShowPopup(true);
     } catch (error) {
